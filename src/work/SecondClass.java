e24bac8fba5eef6eaaeb26cba73f36207fdd4436
Aravind <araravinth14795@gmail.com>
2022-04-28 00:36:08 +0530

    Its my 1st practice commit

---
+++ src/work/SecondClass.java
@@ -0,0 +1,17 @@
+package work;
+
+import org.testng.annotations.Test;
+
+public class SecondClass {
+
+	@Test
+	public void itsSecond() {
+		System.out.println("Hey its 1");
+		System.out.println("Hey its 2");
+	}
+	@Test
+	public void itsSecondSecond() {
+		System.out.println("Hey its 11");
+		System.out.println("Hey its 22");
+	}
+}
---
+++ src/work/firstClass.java
@@ -0,0 +1,19 @@
+package work;
+
+import org.testng.annotations.Test;
+
+public class firstClass {
+	
+	@Test
+	public void itsFirst() {
+		System.out.println("Its me 1");
+		System.out.println("Its me 2");
+	}
+	
+	@Test
+	public void itsFirstSecond() {
+		System.out.println("Its me me 1");
+		System.out.println("Its me me 2");
+	}
+
+}
